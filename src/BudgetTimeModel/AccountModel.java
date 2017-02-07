68b71aa6f85fb5c221b4c6a5eaffbf44ecbb2f29
srgupta5328 <srgupta5328@gmail.com>
2017-02-07 16:01:10 -0500

    PleaseWork

--- src/BudgetTimeModel/AccountModel.java
+++ src/BudgetTimeModel/AccountModel.java
@@ -1,5 +1,34 @@
 package BudgetTimeModel;
 
-public class testmodel {
-
+public class AccountModel{
+	
+	String username; 
+	String password; 
+	int accountID; 
+	
+	public String getUsername(){
+		return username; 
+	}
+	
+	public void setUsername(String userN){
+		this.username = userN; 
+	}
+	
+	public String getPassword(){
+		return password; 
+	}
+	
+	public void setPassword(String pwd){
+		this.password = pwd; 
+	}
+	
+	public int getAccountIDs(){
+		return accountID; 
+	}
+	
+	public void setAccountIDs(int acntID){
+		this.accountID = acntID; 
+	}
+	
 }
+	
