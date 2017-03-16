934b728a4ccd3c534548e9b382b8cadb7fee12de
srgupta5328 <srgupta5328@gmail.com>
2017-03-16 11:44:56 -0400

    Added an arraylist of categories

---
+++ src/BudgetTimeLogic/model/CategoryModel.java
@@ -0,0 +1,24 @@
+package BudgetTimeLogic.model;
+
+
+import java.util.ArrayList;
+
+public class CategoryModel {
+	 
+	 
+	public void categoryModel(){
+		ArrayList <String> category = new ArrayList <String>(); 
+		category.add("Food & Drink");
+		category.add("Gas & Fuel");
+		category.add("Utilities");
+		category.add("General Expenses"); 
+		category.add("Rent");  
+		category.add("Travel");
+		category.add("Subscription"); 
+		
+	}
+	
+	
+
+
+}
