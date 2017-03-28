83126875db6f55b82b1093c5d8c276da125be6c7
srgupta5328 <srgupta5328@gmail.com>
2017-03-28 16:08:03 -0400

    Some more stuff that I added

--- src/BudgetTimeLogic/view/CategoryController.java
+++ src/BudgetTimeLogic/view/CategoryController.java
@@ -1,13 +1,46 @@
 package BudgetTimeLogic.view;
 
+import java.util.ArrayList;
+import java.util.Locale.Category;
 import java.util.Scanner;
 
 public class CategoryController {
 	
 	Scanner userInput = new Scanner (System.in);
-	String category; 
+ 
+	
+	public void categoryModel(){
+		ArrayList <String> category = new ArrayList <String>(); 
+		category.add("Food & Drink");
+		category.add("Gas & Fuel");
+		category.add("Utilities");
+		category.add("General Expenses"); 
+		category.add("Rent");  
+		category.add("Travel");
+		category.add("Subscription"); 
+		
+	}
+	
 	public void addCategory(){
+		ArrayList <String> category  = new <String> ArrayList(); 
+		for (int i = 0; i <50; i++){
+			String e = userInput.nextLine(); 
+			category.add(e);
+		}
 		
-		
+	}
+	
+	
+	public void removeCategory(){
+		ArrayList <String> category = new <String> ArrayList(); 
+		for (int i = 0; i <50; i++){
+			int index = userInput.nextInt();
+			category.remove(index); 
+			
+		}
+	}
+	
+	public void getCategory(){
+		System.out.println(Category.DISPLAY.name());
 	}
 }
